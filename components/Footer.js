9d0ee2b54b8fea179fb833f108dddc52c70371bb
kbcruz6 <agustintcruz@gmail.com>
2022-11-28 16:50:54 -0300

    botones

--- components/Footer.js
+++ components/Footer.js
@@ -7,19 +7,15 @@ export function Footer() {
         href="https://xkcd.com"
         target="_blank"
         rel="noopener noreferrer"
-        className="italic"
+        className=""
       >
-        All comics by xkcd
+        <button className="btn relative inline-flex items-center justify-start overflow-hidden transition-all bg-white rounded hover:bg-white group">
+          <span className="w-0 h-0 rounded bg-purple-600 absolute top-0 left-0 ease-out duration-500 transition-all group-hover:w-full group-hover:h-full -z-1"></span>
+          <span className="w-full text-black transition-colors duration-300 ease-in-out group-hover:text-white z-10 p-2 italic">
+            All comics by xkcd
+          </span>
+        </button>
       </Link>
     </footer>
   );
-}
-
-{
-  /* <button className="btn relative inline-flex items-center justify-start overflow-hidden transition-all bg-white rounded hover:bg-white group">
-  <span className="w-0 h-0 rounded bg-purple-600 absolute top-0 left-0 ease-out duration-500 transition-all group-hover:w-full group-hover:h-full -z-1"></span>
-  <span className="w-full text-black transition-colors duration-300 ease-in-out group-hover:text-white z-10">
-    hover effect 2
-  </span>
-</button>; */
 }
--- pages/comic/[id].js
+++ pages/comic/[id].js
@@ -39,14 +39,18 @@ export default function Comic({
           <p>{alt}</p>
           <div className="flex justify-between mt-4 font-bold">
             {hasPrev && (
-              <Link href={`/comic/${prevId}`} className="text-gray-600">
-                ◀ Previous
+              <Link href={`/comic/${prevId}`}>
+                <button className="btn shadow-[0_5px_0_rgb(0,0,0)] hover:shadow-[0_2px_0px_rgb(0,0,0)] text-black bg-gray-200 ease-out hover:translate-y-1 transition-all rounded pr-2 pl-2">
+                  ← Previous
+                </button>
               </Link>
             )}
 
             {hasNext && (
               <Link className="text-gray-600" href={`/comic/${nextId}`}>
-                Next ▶
+                <button className="btn shadow-[0_5px_0_rgb(0,0,0)] hover:shadow-[0_2px_0px_rgb(0,0,0)] text-black bg-gray-200 ease-out hover:translate-y-1 transition-all rounded pr-2 pl-2">
+                  Next →
+                </button>
               </Link>
             )}
           </div>
@@ -100,17 +104,4 @@ export async function getStaticProps({ params }) {
       nextId,
     },
   };
-}
-
-{
-  /* <button className="btn shadow-[0_5px_0_rgb(0,0,0)] hover:shadow-[0_2px_0px_rgb(0,0,0)] text-black bg-gray-200 ease-out hover:translate-y-1 transition-all rounded pr-2 pl-2"> 
-                ◀ Previous
-                </button> */
-}
-
-{
-  /* <button className="btn shadow-[0_5px_0_rgb(0,0,0)] hover:shadow-[0_2px_0px_rgb(0,0,0)] text-black bg-gray-200 ease-out hover:translate-y-1 transition-all rounded pr-2 pl-2"> 
-                Next ▶
-
-</button> */
 }
