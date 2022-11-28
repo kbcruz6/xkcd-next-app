257b352e950e0c457e4ea3b912d433c1830a0edd
kbcruz6 <agustintcruz@gmail.com>
2022-11-28 17:17:58 -0300

    123123123

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,12 @@ export function Header() {
   return (
     <header className="flex justify-between items-center p-4 max-w-xl m-auto">
       <Link href="/">
-        <h1 className="font-bold hover:text-gray-400">
-          next<span className="font-light"> xkcd</span>
-        </h1>
+        <p className=" relative group font-bold hover:text-gray-400">
+          <span className="text-sm font-bold hover:text-gray-400">
+            next<span className="font-light"> xkcd</span>
+          </span>
+          <span className="absolute -bottom-0 left-0 w-0 h-1 bg-blue-600 transition-all group-hover:w-full"></span>
+        </p>
       </Link>
       <nav>
         <ul className="flex flex-row gap-2">
