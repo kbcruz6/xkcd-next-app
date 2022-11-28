f7b9debacc8896821d1d34ad6555141d1afbabbc
kbcruz6 <agustintcruz@gmail.com>
2022-11-28 16:59:32 -0300

    321321321

--- pages/index.js
+++ pages/index.js
@@ -15,10 +15,10 @@ export default function Home({ latestComics }) {
 
       <Layout>
         <h2 className="text-3xl font-bold text-center mb-10">Latest Comics</h2>
-        <section className="grid grid-cols-1 gap-2 items-center max-w-md m-auto sm:grid-cols-2 md:grid-cols-3 align-middle hover:">
+        <section className="grid grid-cols-1 gap-2 items-center max-w-md m-auto sm:grid-cols-2 md:grid-cols-3 ">
           {latestComics.map((comic) => {
             return (
-              <div className="hover:scale-125 transition duration-500 ">
+              <div key={comic.id} className="hover:scale-125 duration-500 ">
                 <Link href={`/comic/${comic.id}`} key={comic.id} legacyBehavior>
                   <a className="mb-4 pb-4 m-auto">
                     <h3 className="font-bold text-sm text-center pb-2">
